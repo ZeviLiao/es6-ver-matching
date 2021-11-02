389800ebc04e6281437d1ea7eba221cb8ea9e965
Zevi Liao <jointjm@gmail.com>
2021-11-02 21:39:53 +0800

    get max typecode

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import { checkApk, checkMbkx, checkMbtx } from './utils/capability'
 import rsp from './matching-version/res.json'
 import semver from 'semver'
 
-
 rsp.data.map(d => {
     // console.log(d)
     // each device for apk.
     // console.log(checkApk(d))
-    console.log(checkMbkx(d))
-    console.log(checkMbkx(d))
+    let list = []
+    //same device.clientId
+    list.push(checkApk(d))
+    list.push(checkMbkx(d))
+    list.push(checkMbkx(d))
+
+    const max = list.reduce((prev, current) => (
+        (prev.typeCode > current.typeCode) ? prev : current)
+    )
+
+    console.log(max);
 })
 
 
