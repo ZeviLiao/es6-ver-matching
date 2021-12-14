c1b533d8d4047408fb7a7eb2f4d37534bff02b4f
Zevi Liao <jointjm@gmail.com>
2021-12-14 14:36:46 +0800

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const checkTypes = {
     'mbkx': 0,
     'mbtx': 0,
     'media': 1,
-    
 }
 let cmdTemplate = []
 let fArr = []
@@ -16,13 +15,10 @@ Object.keys(checkTypes).forEach(type => {
     if (checkTypes[type] === 1) {
         let content = getJsonDoc(type)
         content.forEach(element => {
-            // console.log(element.type)
             if (element.type === 'cmdCapabilityJson') {
-                // console.log(element.featureList)
                 fArr = fArr.concat(element.featureList)
             }
             if (element.type === 'cmdPackageExist') {
-                // console.log(element)
                 pArr.push(element)
             }
         });
@@ -32,12 +28,8 @@ Object.keys(checkTypes).forEach(type => {
 fArr.sort(function (a, b) {
     var nameA = a.dependenciesVersion; // ignore upper and lowercase
     var nameB = b.dependenciesVersion; // ignore upper and lowercase
-    if (nameA < nameB) {
-        return -1;
-    }
-    if (nameA > nameB) {
-        return 1;
-    }
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
 
     // names must be equal
     return 0;
@@ -54,11 +46,3 @@ cmdTemplate.push(...new Map(pArr.map(v => [v.packageName, v])).values())
 
 console.log(JSON.stringify(cmdTemplate, null, 4))
 // console.log(cmdTemplate)
-
-
-
-
-
-
-
-
